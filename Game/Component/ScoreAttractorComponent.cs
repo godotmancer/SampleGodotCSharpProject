b096eb04e2a5a86c2df36cc0eda2eeb6415d519f
godotmancer <godotmancer@gmail.com>
2023-02-16 17:18:49 +0200

    more Zombie renames

---
+++ Game/Component/ScoreAttractorComponent.cs
@@ -0,0 +1,10 @@
+namespace SampleGodotCSharpProject.Game.Component;
+
+public partial class ScoreAttractorComponent : BaseComponent
+{
+    public override void _Ready()
+    {
+        var tween = CreateTween();
+        
+    }
+}
